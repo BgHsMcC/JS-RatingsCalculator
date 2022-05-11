dfc449e2a920638cf874089f3f7d515e8da3c993
Remya Mohanan <remyamohanan91@gmail.com>
2022-05-10 19:37:11 -0700

    Added JS file

--- js/ratings.js
+++ js/ratings.js
@@ -1,9 +1,11 @@
 function collect_ratings(){
 
-    const ratings = new Object();
-    ratings.count = 0;
-    ratings.sum = 0;
-    ratings. average = 0;
+    const ratings = {
+
+    count : 0,
+    sum: 0,
+    average: 0
+    }
 
     let rating = 0;
 
@@ -11,24 +13,27 @@ function collect_ratings(){
 
     elements.forEach(function(element){
 
-        element.replace('star',"");
-        rating = parseInt(element);
+        
+        rating = parseInt(element.id.replace('star',""));
         ratings.count += parseInt(element.value);
-        ratings.sum += parseInt(element.value);
+        ratings.sum += rating * parseInt(element.value);
 
         });
 
         if(ratings.count !== 0){
-            ratings.average = ratings.sum / ratings.count;
+            ratings.average = (ratings.sum / ratings.count).toFixed();
         }
+        console.log("First project!!!!");
 
         return ratings;
 
     }
 
-    rating.addEventListener('change', function(){
+    document.addEventListener('change', function(){
         const ratings = collect_ratings();
-        const final = document.querySelector('average');
-        final.value = ratings.average.toFixed();
-
+        const final_val = document.querySelector("#average");
+        final_val.value = ratings.average;
+        console.log (final_val.value); 
+       
+    
     });
